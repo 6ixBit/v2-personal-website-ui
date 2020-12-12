cbaececf95f24aa5c0bcbe115a5c6104c9c8523f
Hamza Carew <hamzacarew@gmail.com>
2020-12-12 21:17:42 +0000

    Initial setup for component setup of UI

---
+++ src/components/Grid.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <div>
+       Grid goes here. 
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="">
+    
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Welcome to my website...</h1>
+    <Grid/>
+</template>
+
+<script>
+import Grid from "@/components/Grid.vue"
+export default {
+    name: "Home",
+    components: {
+        Grid
+    }
+}
+</script>
+
+<style lang="sass">
+
+</style>
