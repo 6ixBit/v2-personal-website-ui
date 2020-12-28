591a80479bc31c68197867a5f959df121302b4ba
Hamza Carew <hamzacarew@gmail.com>
2020-12-28 15:43:20 +0000

    Implemented template for contact form

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,50 @@
+<template>
+   <form>
+       <div>
+            <label for="name" class="user-input-label"> > Name</label>
+            <input type="text" name="name" class="user-input">
+       </div>
+
+       <div>
+           <label for="email" class="user-input-label"> > Email</label>
+           <input type="text" name="email" class="user-input">
+       </div>
+
+       <div>
+           <label for="subject" class="user-input-label"> > Subject</label>
+           <input type="text" name="subject" class="user-input">
+       </div>
+
+       <div>
+           <label for="message" class="user-input-label"> > Message</label>
+           <textarea class="block rounded my-2 px-2 h-48 w-60 border-2 border-blue-300"></textarea>
+       </div>
+
+       <div class="flex justify-center">
+           <button class="rounded-full py-2 px-6 my-6 uppercase font-bold font-mono cursor-pointer border-blue-300 border-2">
+               Send
+           </button>
+       </div>
+   </form>
+</template>
+
+<script>
+// import axios from 'axios'
+
+export default {
+   name: "ContactForm",
+   setup() {
+
+   }
+}
+</script>
+
+<style>
+   .user-input {
+       @apply block my-2 px-2 rounded border-solid border-2 border-blue-300 w-60;
+    } 
+
+    .user-input-label {
+       @apply font-mono font-bold; 
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,40 @@
 <template>
-    <div class="flex justify-center">
-        <!-- On mobile make it full width and on larger devices make it 3 quarters of width -->
-        <div class="flex justify-center w-10/12 sm:w-8/12 my-8" id="content"> 
-          <ul id="menu">
-            <li>Home</li>
-            <li>Projects</li>
-          </ul>
+  <div class="flex justify-center">
+    <!-- On mobile make it full width and on larger devices make it 3 quarters of width -->
+    <div class="flex justify-center w-10/12 sm:w-8/12 my-8" id="content">
+      <ul id="menu">
+        <li>Home</li>
+        <li>Projects</li>
+      </ul>
+    </div>
+  </div>
 
-        
-
-        </div>
+   <div class="flex justify-center">
+        <ContactForm/>
     </div>
 </template>
 
 <script>
 // import Grid from "@/components/Grid.vue"
-// import axios from 'axios'
+
+import ContactForm from "@/components/ContactForm.vue";
 
 export default {
-    name: "Home",
-    setup() {
-
-    }
-}
+  name: "Home",
+  components: {
+    ContactForm
+  },
+  setup() {}
+};
 </script>
 
 <style scoped>
 #menu {
-    list-style-type: lower-roman;
-    margin: 30px 0px;
+  list-style-type: lower-roman;
+  margin: 30px 0px;
 }
 
 #content {
-    border: solid blue 2px;
+  border: solid blue 2px;
 }
-
 </style>
