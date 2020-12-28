5a7affa62c3408edccde90659a6b3f862a1fa559
Hamza Carew <hamzacarew@gmail.com>
2020-12-28 15:59:18 +0000

    Removed redundant border used for testing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-center">
     <!-- On mobile make it full width and on larger devices make it 3 quarters of width -->
-    <div class="flex justify-center w-10/12 sm:w-8/12 my-8" id="content">
+    <div class="flex justify-center w-10/12 sm:w-8/12 my-8">
       <ul id="menu">
         <li>Home</li>
         <li>Projects</li>
@@ -34,7 +34,4 @@ export default {
   margin: 30px 0px;
 }
 
-#content {
-  border: solid blue 2px;
-}
 </style>
