e05d6c0ebee585bbf35b92fef269aa434cefd87f
Hamza Carew <hamzacarew@gmail.com>
2020-12-29 17:53:50 +0000

    added data binding to contact form elements

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,27 +1,30 @@
 <template>
-   <form>
+   <form @submit.prevent>
        <div>
             <label for="name" class="user-input-label"> > Name</label>
-            <input type="text" name="name" class="user-input">
+            <input v-model="name" type="text" name="name" class="user-input">
        </div>
 
        <div>
            <label for="email" class="user-input-label"> > Email</label>
-           <input type="text" name="email" class="user-input">
+           <input v-model="email" type="text" name="email" class="user-input">
        </div>
 
        <div>
            <label for="subject" class="user-input-label"> > Subject</label>
-           <input type="text" name="subject" class="user-input">
+           <input v-model="subject" type="text" name="subject" class="user-input">
        </div>
 
        <div>
            <label for="message" class="user-input-label"> > Message</label>
-           <textarea class="block rounded my-2 px-2 h-48 w-60 border-2 border-blue-300"></textarea>
+           <textarea v-model="message" class="block rounded my-2 px-2 h-48 w-60 border-2 border-blue-300"></textarea>
        </div>
 
        <div class="flex justify-center">
-           <button class="rounded-full py-2 px-6 my-6 uppercase font-bold font-mono cursor-pointer border-blue-300 border-2">
+           <button 
+           
+           @click="submitForm"
+           class="rounded-full py-2 px-6 my-6 uppercase font-bold font-mono cursor-pointer bg-blue-300 hover:bg-green-300">
                Send
            </button>
        </div>
@@ -30,11 +33,25 @@
 
 <script>
 // import axios from 'axios'
+import { ref } from 'vue'
 
 export default {
    name: "ContactForm",
    setup() {
 
+       let name, subject, email, message = ref("")
+
+       function submitForm() {
+           console.log("Make POST request to server.")
+       }
+
+       return {
+           name,
+           subject,
+           email,
+           message,
+           submitForm
+       }
    }
 }
 </script>
