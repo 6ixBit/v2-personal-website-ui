78f50878d1c4dc43615d833e97d93f08ad7ac101
Hamza Carew <hamzacarew@gmail.com>
2020-12-29 20:35:35 +0000

    Added fixed sidebar for media svg icons; Implemented navbar for home page

---
+++ src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+   <nav class="bg-gray-800 h-14">
+       <div class="flex items-baseline space-x-4 py-4 px-2 text-white font-mono justify-center">
+           <a href="" class="nav-items">Home</a>
+           <a href="" class="nav-items">Projects</a>
+           <a href="" class="nav-items">Contact</a>
+       </div>
+   </nav>
+</template>
+
+<script>
+export default {
+   name: "NavBar" 
+}
+</script>
+
+<style>
+   .nav-items {
+       @apply hover:bg-gray-700 font-medium rounded-md px-3;
+   } 
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,37 @@
 <template>
+  <NavBar></NavBar>
+
+  <div class="fixed my-4 flex flex-col items-center">
+    <img src="@/assets/images/github.svg"   alt="github"   class="svg-icon">
+    <img src="@/assets/images/linkedin.svg" alt="linkedin" class="svg-icon">
+  </div>
+
   <div class="flex justify-center">
     <!-- On mobile make it full width and on larger devices make it 3 quarters of width -->
     <div class="flex justify-center w-10/12 sm:w-8/12 my-8">
       <ul id="menu">
-        <li>Home</li>
-        <li>Projects</li>
+        <!-- <li>Home</li>
+        <li>Projects</li> -->
       </ul>
     </div>
   </div>
 
    <div class="flex justify-center">
         <ContactForm/>
-    </div>
+   </div>
+
 </template>
 
 <script>
-// import Grid from "@/components/Grid.vue"
 
 import ContactForm from "@/components/ContactForm.vue";
+import NavBar from "@/components/NavBar.vue"
 
 export default {
   name: "Home",
   components: {
-    ContactForm
-  },
-  setup() {}
+    ContactForm, NavBar
+  }
 };
 </script>
 
@@ -34,4 +41,7 @@ export default {
   margin: 30px 0px;
 }
 
+.svg-icon {
+  @apply my-1 mx-1 h-10 cursor-pointer;
+}
 </style>
